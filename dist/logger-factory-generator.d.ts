e06ca843a4285c72ecaa0d466d6cbffc3aeeb623
Rodrigo Antônio de Araújo <rodjjo@gmail.com>
2019-07-30 15:32:22 -0300

    Generate dist

--- dist/logger-factory-generator.d.ts
+++ dist/logger-factory-generator.d.ts
@@ -5,14 +5,22 @@ declare interface IConfig {
         dsn?: string;
         level?: string;
     };
+    logstash?: {
+        enabled?: boolean;
+        application?: string;
+        host?: string;
+        port?: number;
+        level?: string;
+    };
 }
 export interface IFactoryInterface {
     config: IConfig;
 }
 declare type LoggerFactoryType = ({ config }: IFactoryInterface) => Logger;
-export declare const loggerFactoryGenerator: ({ winston, consoleTransportClass, sentryTransportClass }: {
+export declare const loggerFactoryGenerator: ({ winston, consoleTransportClass, sentryTransportClass, logstashTransportClass }: {
     winston: any;
     consoleTransportClass: any;
     sentryTransportClass: any;
+    logstashTransportClass: any;
 }) => LoggerFactoryType;
 export {};
