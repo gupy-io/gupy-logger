69ddf7263e1a1e93f148af0a23d39fc3b5c7dcec
Nando Ferraz <nandooferraz@gmail.com>
2021-07-08 15:03:00 -0300

    Remove use of sentry and logstash transports

--- src/logger-factory-generator.ts
+++ src/logger-factory-generator.ts
@@ -1,109 +1,80 @@
-import {Logger} from 'winston';
+import { Logger } from 'winston';
 import * as moment from 'moment';
 
 const DATETIME_FORMAT = 'YYYY-MM-DD HH:mm:ss.SSS Z';
 
 declare interface IConfig {
-    level?: string;
-    sentry: {
-        enabled: boolean,
-        dsn?: string,
-        level?: string,
-        sampleRate?: number,
-    };
-    logstash?: {
-        enabled?: boolean,
-        application?: string,
-        host?: string,
-        port?: number,
-        level?: string,
-    };
+  level?: string;
+  sentry: {
+    enabled: boolean,
+    dsn?: string,
+    level?: string,
+    sampleRate?: number,
+  };
+  logstash?: {
+    enabled?: boolean,
+    application?: string,
+    host?: string,
+    port?: number,
+    level?: string,
+  };
 }
 
 function prepareErrorToLog(error, messages = []) {
-    if (messages.length) {
-        error.message = `${error.message} :: ${messages.join(',')}`;
-    }
-    return error;
+  if (messages.length) {
+    error.message = `${error.message} :: ${messages.join(',')}`;
+  }
+  return error;
 }
 
 export interface IFactoryInterface {
-    config: IConfig;
+  config: IConfig;
 }
 
 type LoggerFactoryType = ({ config }: IFactoryInterface) => Logger;
 
-export const loggerFactoryGenerator = ({winston, consoleTransportClass, sentryTransportClass, logstashTransportClass}): LoggerFactoryType => {
-    return ({config}: IFactoryInterface) => {
-        const transports = [];
-        transports.push(new consoleTransportClass({
-            level: config.level,
-        }));
+export const loggerFactoryGenerator = ({
+  winston,
+  consoleTransportClass,
+  sentryTransportClass,
+  logstashTransportClass,
+}): LoggerFactoryType => ({ config }: IFactoryInterface) => {
+  const transports = [];
+  transports.push(new consoleTransportClass({
+    level: config.level,
+  }));
 
-        if (config.sentry.enabled) {
-            transports.push(new sentryTransportClass({
-                dsn: config.sentry.dsn,
-                level: 'error',
-                config: {
-                    sampleRate: config.sentry.sampleRate || 0.25
-                }
-            }));
-        }
+  const logger: Logger = winston.createLogger({
+    format: winston.format.printf(
+      error => `${moment.utc().format(DATETIME_FORMAT)} [${error.level}]: ${error.message}`,
+    ),
+    transports,
+    exitOnError: false,
+  });
 
-        if (config.logstash && config.logstash.enabled && logstashTransportClass) {
-            const appendMetaInfo = winston.format((info) => {
-                return Object.assign(info, {
-                  application: config.logstash.application || 'gupy',
-                  pid: process.pid,
-                  time: moment.utc().format(DATETIME_FORMAT),
-                });
-            });
+  const errorFn = logger.error;
 
-            transports.push(new logstashTransportClass({
-                host: config.logstash.host,
-                port: config.logstash.port,
-                level: config.level,
-                format: winston.format.combine(
-                    appendMetaInfo(),
-                    winston.format.json(),
-                    winston.format.timestamp()
-                ),
-            }));
-        }
+  logger.error = (...args) => {
+    if (!args || !args.length) return;
 
-        const logger: Logger = winston.createLogger({
-            format: winston.format.printf(
-                error => `${moment.utc().format(DATETIME_FORMAT)} [${error.level}]: ${error.message}`,
-            ),
-            transports,
-            exitOnError: false,
-        });
+    let error;
+    const messages = [];
+    let object = {};
 
-        const errorFn = logger.error;
+    args.forEach((arg) => {
+      if (arg instanceof Error) {
+        error = arg;
+      } else if (typeof arg === 'string') {
+        messages.push(arg);
+      } else if (typeof arg === 'object') {
+        object = { ...object, ...arg };
+      }
+    });
 
-        logger.error = (...args) => {
-            if (!args || !args.length) return;
-
-            let error;
-            const messages = [];
-            let object = {};
-
-            args.forEach((arg) => {
-                if (arg instanceof Error) {
-                    error = arg;
-                } else if (typeof arg === 'string') {
-                    messages.push(arg);
-                } else if (typeof arg === 'object') {
-                    object = { ...object, ...arg };
-                }
-            });
-
-            if (error) {
-                return errorFn(prepareErrorToLog(error, messages), {...object, stack: error.stack});
-            } else {
-                return errorFn.apply(logger, args);
-            }
-        };
-        return logger;
-    };
+    if (error) {
+      return errorFn(prepareErrorToLog(error, messages), { ...object, stack: error.stack });
+    }
+    return errorFn.apply(logger, args);
+  };
+  return logger;
 };
